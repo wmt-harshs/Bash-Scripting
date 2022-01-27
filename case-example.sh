1f042ab0c689b08dca6f4f1f0622cb55cb1e5449
wmt-harshs <harshs@webmob.tech>
2022-01-27 16:47:06 +0530

    script using case statement

---
+++ case-example.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+echo "Enter your lucky number"
+read n
+case $n in
+
+101)
+echo "you got 1st prize";;
+
+510)
+echo "you got 2nd prize";;
+
+999)
+echo "you got 3rd prize";;
+
+*)
+echo "Sorry,try for the next time"
+esac
+
+
+exit
